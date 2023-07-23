8500dbf2623fcfd090f954eb7f639bb23aec13ec
[Jeremy] <[salinasjeremy2@gmail.com]>
2023-07-22 22:06:10 -0500

    made code shorter

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ function mute() {
   let audioIcon = document.getElementById("muteAudio");
   audio.muted = !audio.muted;
 
-  if (audioIcon.alt == "unmuted") {
-    audioIcon.src = "source/img/muted.png";
-    audioIcon.alt = "muted";
-  } else {
-    audioIcon.src = "source/img/unmuted.png";
-    audioIcon.alt = "unmuted";
-  }
+  audioIcon.src =
+    audioIcon.alt === "unmuted"
+      ? "source/img/muted.png"
+      : "source/img/unmuted.png";
+  audioIcon.alt = audioIcon.alt === "unmuted" ? "muted" : "unmuted";
 }
 
 const togglePlayer = (playerNumber) => {
@@ -21,13 +19,11 @@ const togglePlayer = (playerNumber) => {
   let opponentText =
     document.getElementsByClassName("opponent-text")[playerNumber];
 
-  if (opponentSwitchImg.alt == "humen") {
-    opponentSwitchImg.src = "source/img/bot.png";
-    opponentSwitchImg.alt = "bot";
-    opponentText.textContent = "bot";
-  } else {
-    opponentSwitchImg.src = "source/img/humen.png";
-    opponentSwitchImg.alt = "humen";
-    opponentText.textContent = "player";
-  }
+  opponentSwitchImg.src =
+    opponentSwitchImg.alt === "humen"
+      ? "source/img/bot.png"
+      : "source/img/humen.png";
+  opponentSwitchImg.alt = opponentSwitchImg.alt === "humen" ? "bot" : "humen";
+  opponentText.textContent =
+    opponentSwitchImg.alt === "humen" ? "bot" : "player";
 };
