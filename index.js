f94ee7685fab13a50b5099b37dcf8f74ee45bfa5
[Jeremy] <[salinasjeremy2@gmail.com]>
2023-07-22 21:51:41 -0500

    added audio

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+const audio = document.getElementById("backgroundMusic");
+audio.volume = 0.05;
+
+function mute() {
+  let audio = document.getElementById("backgroundMusic");
+  let audioIcon = document.getElementById("muteAudio");
+  audio.muted = !audio.muted;
+
+  if (audioIcon.alt == "unmuted") {
+    audioIcon.src = "source/img/muted.png";
+    audioIcon.alt = "muted";
+  } else {
+    audioIcon.src = "source/img/unmuted.png";
+    audioIcon.alt = "unmuted";
+  }
+}
+
 const togglePlayer = (playerNumber) => {
-  let opponentSwitchImg = document.getElementsByClassName("opponent-img")[playerNumber];
-  let opponentText = document.getElementsByClassName("opponent-text")[playerNumber];
+  let opponentSwitchImg =
+    document.getElementsByClassName("opponent-img")[playerNumber];
+  let opponentText =
+    document.getElementsByClassName("opponent-text")[playerNumber];
 
   if (opponentSwitchImg.alt == "humen") {
     opponentSwitchImg.src = "source/img/bot.png";
